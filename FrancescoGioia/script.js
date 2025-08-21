795a67839fbfe5d359df9f762d2ce94792607468
manan294 <mananp294@gmail.com>
2025-08-21 09:12:57 +0530

    updated

---
+++ FrancescoGioia/script.js
@@ -0,0 +1,29 @@
+var tl = gsap.timeline()
+
+
+
+tl.from("#nav",{
+    opacity: 0,
+    duration: 1,
+    ease: "none",
+    y: -50,
+    // stagger:1
+})
+
+tl.from("#banner h1",{
+    opacity: 0,
+    stagger:0.5,
+    duration: 0.7,
+    ease: "none",
+    x: -70,
+}, )
+
+
+tl.from("#banner img",{
+    opacity: 0,
+    x: 100,
+    rotate:45,
+    duration: 0.5,
+    ease: "none",
+    stagger: 0.5,
+} )
